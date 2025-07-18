43752b8fcd917a9d4286af94058e491a5ecb7cc2
Pavel Gritskov <pavel.gri.dev@gmail.com>
2025-07-18 16:17:35 +0300

    Initial Commit

---
+++ TestApp_JustMobi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestApp_JustMobi
+//
+//  Created by Pavel Gritskov on 18.07.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
